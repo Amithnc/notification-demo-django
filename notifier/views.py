43b28c242b262748d57cf2a83e0154ccfad1014a
Amithnc <53530365+Amithnc@users.noreply.github.com>
2022-01-03 11:39:20 +0530

    added twillo for sending messages

--- notifier/views.py
+++ notifier/views.py
@@ -1,6 +1,29 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from decouple import config
+import smtplib
+from twilio.rest import Client
 
 # Create your views here.
-def homepage(request):
-    return HttpResponse("hi")
+def send_notification(request):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    Emailid = config('EmailId')
+    password= config('Password')
+    server.login(Emailid, password)
+    msg = "hello :)"
+    subject = " Demo Mail"
+    body = "Subject: {}\n\n{}".format(subject,msg)
+    server.sendmail(Emailid, 'amith.nc@apisero.com', body)
+    
+
+    #for message:
+    account_sid = 'ACd8699836ee0052a11d61d2dfdf5643df'
+    auth_token = config('AuthToken')
+    client = Client(account_sid, auth_token) 
+    message = client.messages.create(  
+                              messaging_service_sid='MG3b3e70c06ee9facda0ff8abaf7175875', 
+                              body='Hello',      
+                              to='+919110673198' ) 
+    print(message.sid)
+    return HttpResponse("notification sent")
